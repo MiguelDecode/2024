e682a48786dfeb1433c5bb1a3a10dde2835cf9a7
Miguel Decode <migueldecode@gmail.com>
2024-06-03 18:52:21 +0200

    study: react hook useState

---
+++ 05-2024/aprendiendo-react/projects/02-tic-tac-toe/src/logic/board.js
@@ -0,0 +1,14 @@
+import { WINNER_COMBOS } from "../constants";
+
+export const checkWinner = (board) => {
+  for (const combo of WINNER_COMBOS) {
+    const [a, b, c] = combo;
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return board[a]; // x or o
+    }
+  }
+
+  return null;
+};
+
+export const checkEndGame = (board) => board.every((square) => square !== null);
